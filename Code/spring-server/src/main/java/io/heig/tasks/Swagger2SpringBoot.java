fd4cb8a6364c022d8e79f9e2895875e1cec48764
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-11-15 16:21:46 +0100

    functionnal project

--- Code/spring-server/src/main/java/io/heig/tasks/Swagger2SpringBoot.java
+++ Code/spring-server/src/main/java/io/heig/tasks/Swagger2SpringBoot.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.avalia.fruits", "io.avalia.fruits.api" })
+@ComponentScan(basePackages = { "io.heig.tasks", "io.heig.tasks.api" })
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
