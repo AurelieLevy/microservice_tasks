9abf2559dd9ae6af69e5307cf7012d605cf06b76
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-11-24 16:28:42 +0100

    A finir cf le todo

--- Code/tasks-specs/src/test/java/io/heig/tasks/api/spec/steps/CreationTasksTests.java
+++ Code/tasks-specs/src/test/java/io/heig/tasks/api/spec/steps/CreationTasksTests.java
@@ -1,5 +1,8 @@
 package io.heig.tasks.api.spec.steps;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -7,14 +10,19 @@ import cucumber.api.java.en.When;
 import io.heig.tasks.ApiException;
 import io.heig.tasks.ApiResponse;
 import io.heig.tasks.api.TaskApi;
+import io.heig.tasks.api.dto.NewTask;
 import io.heig.tasks.api.dto.Task;
 import io.heig.tasks.api.spec.helpers.Environment;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Olivier Liechti on 27/07/17.
@@ -83,8 +91,40 @@ public class CreationTasksTests {
 
     @Given("^I have an invalid  type payload \\(not JSON\\)$")
     public void i_have_an_invalid_type_payload_not_JSON() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        /*NewTask newTask = new NewTask();
+        newTask.setName("fdsf");
+        newTask.setDescription("lolilol");
+        api.postTaskWithHttpInfo(newTask);
+
+        lastApiResponse = api.getTasksWithHttpInfo();
+        assertEquals(406, lastApiResponse.getStatusCode());*/
+
+        URL url = new URL(api.getApiClient().getBasePath());
+        String badPayload = "aaaaaaaa";
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+
+        connection.setRequestProperty("Content-length", String.valueOf((Integer)badPayload.getBytes().length));
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes(badPayload);
+        wr.close();
+
+        InputStream is = connection.getInputStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while((line = rd.readLine()) != null){
+            response.append(line);
+        }
+
+        rd.close();
+
+        //TODO
+
     }
 
     @Given("^I have an JSON payload with incorrect parameters$")
@@ -95,8 +135,19 @@ public class CreationTasksTests {
 
     @When("^I GET to the /tasks/TASK_ID endpoint$")
     public void i_GET_to_the_tasks_TASK_ID_endpoint() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        try {
+            lastApiResponse= api.getTaskByIdWithHttpInfo("1");
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+            assertNotNull(lastStatusCode);
+            assertEquals(200, lastStatusCode);
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
     }
 
 }
