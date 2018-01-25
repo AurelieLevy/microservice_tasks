18469e771461dbd18e7fd7dac79a7abd7fa62be9
Mathias Gilson <mathias.gilson@outlook.com>
2018-01-25 22:35:30 +0100

    Added task repo method

--- Code/spring-server/src/main/java/io/heig/tasks/repositories/TaskRepository.java
+++ Code/spring-server/src/main/java/io/heig/tasks/repositories/TaskRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface TaskRepository extends MongoRepository<TaskEntity, String>{
-
+    TaskEntity findOneByTaskId(String taskId);
 }
