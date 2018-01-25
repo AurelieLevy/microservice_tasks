672840dcfe5d7bbcdf8337f9fdf364945399d815
thibaud.besseau@gmail.com <wNFSetlKk!Vc'EGh9W7GKvQ*cefKnhzQ>
2018-01-25 22:59:12 +0100

    add some security check

--- Code/spring-server/src/main/java/io/heig/tasks/api/endpoints/ExecutionApiController.java
+++ Code/spring-server/src/main/java/io/heig/tasks/api/endpoints/ExecutionApiController.java
@@ -60,17 +60,29 @@ public class ExecutionApiController implements ExecutionsApi {
         e = ExecutionRepository.insert(e);
         TaskEntity taskEntity =  taskRepository.findOneByTaskId(body.getTaskId());
 
-        ArrayList<ExecutionEntity> executions;
-        if(taskEntity.getExecutions() == null) {
-            executions = new ArrayList<>();
-        } else {
-            executions = taskEntity.getExecutions();
+        if(taskEntity != null)
+        {
+            ArrayList<ExecutionEntity> executions;
+            if (taskEntity.getExecutions() == null)
+            {
+                executions = new ArrayList<>();
+            }
+            else
+            {
+                executions = taskEntity.getExecutions();
+            }
+
+            executions.add(e);
+            taskEntity.setExecutions(executions);
+
+            taskRepository.save(taskEntity);
+            return new ResponseEntity<Execution>(e.getDTO(), HttpStatus.CREATED);
+        }
+        else
+        {
+            return new ResponseEntity<Execution>(e.getDTO(), HttpStatus.NOT_FOUND);
+
         }
 
-        executions.add(e);
-        taskEntity.setExecutions(executions);
-
-        taskRepository.save(taskEntity);
-        return new ResponseEntity<Execution>(e.getDTO(), HttpStatus.CREATED);
     }
 }
