5fdf92badbede4d17a3020c5e6b99ea264e21be0
thibaud.besseau@gmail.com <wNFSetlKk!Vc'EGh9W7GKvQ*cefKnhzQ>
2018-01-26 14:13:22 +0100

    add docker files

---
+++ build-image-server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "---- Building tasks-api-server -----"
+cd swagger/spring-server/
+mvn clean install
+
+echo "---- Copying  tasks-api-server.jar ----"
+cp /target/tasks-api-server-1.0.0.jar  ../../images/tasks-api-server-1.0.0.jar 
+
+cd ../../images/
+
+echo "---- Building the task image -----"
+docker build -t lozzikit/tasks .
